92befa1bc145d3d9a1003d00f0ec60291c610ab3
jacpete <jacobmpeterson12@gmail.com>
2021-02-16 14:02:49 -0800

    update onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,21 @@
 .onLoad <- function(libname, pkgname) {
-  reticulate::configure_environment(pkgname)
+  # reticulate::configure_environment(pkgname)
 
-  pyPackages <- c('numpy', 'pandas')
-  reticulate_check_dependencies(pyPackages)
+  #check for miniconda
+  if (!reticulate_miniconda_exists()) {
+
+    promptResp <- readline(prompt = "No installation of miniconda found. Would you like to install it? y/n: ")
+    promptResp <- tolower(promptResp)
+
+    if (identical(promptResp, 'y')) {
+      reticulate::install_miniconda()
+    }
+  }
+
+  #install python dependencies
+  if (reticulate_miniconda_exists()) {
+    pyPackages <- c('numpy', 'pandas')
+    reticulate_check_dependencies(pyPackages)
+  }
+
 }
